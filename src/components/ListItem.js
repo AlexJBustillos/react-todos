d2e282550eea8913b27c2408a1b5642c739be8a2
alexjbustillos <alexjbustillos@gmail.com>
2020-12-08 17:23:46 -0500

    [class]component used to practice

--- src/components/ListItem.js
+++ src/components/ListItem.js
@@ -1,8 +1,20 @@
-function ListItem(props) {
-    return (
-        <li>{props.doThis}</li>
+import React, { Component } from 'react';
+// using function components
+// function ListItem(props) {
+//     return (
+//         <li>{props.doThis}</li>
         
-    )
-}
+//     )
+// }
 
+//using class component
+class ListItem extends Component {
+    render() {
+        return(
+            <div>
+                <li>{this.props.doThis}</li>
+            </div>
+        )
+    }
+}
 export default ListItem;
